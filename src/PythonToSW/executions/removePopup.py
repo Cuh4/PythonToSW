83845f2989efb02d5a64c4a89bf2e0327cdc51c6
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2024-05-14 17:30:15 +0100

    change project description

--- src/PythonToSW/executions/removePopup.py
+++ src/PythonToSW/executions/removePopup.py
@@ -2,7 +2,7 @@
 # [PythonToSW] Remove Popup
 # ----------------------------------------
 
-# A Python package that allows you to execute server functions in a Stormworks: Build and Rescue addon.
+# A Python package that allows you to make Stormworks addons with Python.
 # Repo: https://github.com/Cuh4/PythonToSW • Creator: Cuh4 • License: Apache 2.0
 
 # ---- // Imports
--- src/PythonToSW/matrix.py
+++ src/PythonToSW/matrix.py
@@ -2,7 +2,7 @@
 # [PythonToSW] Matrix
 # ----------------------------------------
 
-# A Python package that allows you to execute server functions in a Stormworks: Build and Rescue addon.
+# A Python package that allows you to make Stormworks addons with Python.
 # Repo: https://github.com/Cuh4/PythonToSW • Creator: Cuh4 • License: Apache 2.0
 
 # ---- // Main
