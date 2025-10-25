de40af9e6872d5f2449d95020208dc732a92f0cf
Cuh4 <89425262+Cuh4@users.noreply.github.com>
2025-10-25 18:16:42 +0100

    feat(examples/CustomLuaCode): showcase new custom callback invoking

--- examples/CustomLuaCode/code.lua
+++ examples/CustomLuaCode/code.lua
@@ -1,8 +1,22 @@
-foo = {}
-foo.bar = {}
+--[[
+    This code simply sends up a callback ("foo")
+    when the PythonToSW server connects.<br>
+
+    It also provides a custom function ("Foo.MyFunction") which
+    is called from the Python side.
+]]
+
+---@class Foo: NoirService
+Foo = Noir.Services:CreateService("Foo")
+
+function Foo:ServiceStart()
+    SWToPython.Uplink.OnConnect:Connect(function()
+        SWToPython.Uplink:InvokeCallback("foo", {"Hello from in-game!"})
+    end)
+end
 
 ---@param time number
-function foo.bar.myFunction(time)
+function Foo.MyFunction(time)
     print("I was called! Time provided by the Python side: "..time)
     return 1
 end
--- examples/CustomLuaCode/main.py
+++ examples/CustomLuaCode/main.py
@@ -1,12 +1,25 @@
+"""
+An example showcasing Lua code injection.
+
+When the addon starts, `code.lua` is injected into the in-game addon's code.
+
+The code contains a function which we call through Python.
+The code also invokes a callback that we can connect to.
+"""
+
+import os
 import time
 
 from PythonToSW import (
     Addon
 )
 
+# Just so paths work regardless of current working directory
+SELF_PATH = os.path.dirname(__file__)
+
 addon = Addon(
     "Testing",
-    path = ".",
+    path = SELF_PATH,
     port = 2000
 )
 
@@ -15,8 +28,8 @@ def on_start():
     Called when the addon starts (connects with in-game addon).
     """
     
-    # Call `foo.bar.myFunction` with arguments `time.time()`
-    number, = addon.call_function("foo.bar.myFunction", time.time())
+    # Call `Foo.MyFunction` with arguments `time.time()`
+    number, = addon.call_function("Foo.MyFunction", time.time())
     
     # ...and print what was returned:
     print(f"We got: {number}")
@@ -28,9 +41,19 @@ def on_stop():
     
     pass
 
+def on_foo(message: str):
+    """
+    Custom callback triggered by the custom Lua code.
+    """
+    
+    print(f"Foo called! Message: {message}")
+
 # Inject custom Lua code to be ran in-game
-addon.attach_lua_file("code.lua")
+addon.attach_lua_file(os.path.join(SELF_PATH, "code.lua"))
 # addon.attach_lua_code("print('Hello world!')") # Also works
+
+# Listen for custom callback
+addon.connect("foo", on_foo)
 
 # Start the addon
 # `on_start` is called when the in-game addon connects
